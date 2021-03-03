60cf7c4ff6003caa4669b112c14f3a9d63bf38bb
Robert Yip <robertyip88@gmail.com>
2021-03-03 00:01:14 -0500

    Phase 2 corrected names and comments of tests

--- src/test/persistence/TestJsonWriter.java
+++ src/test/persistence/TestJsonWriter.java
@@ -14,7 +14,6 @@ public class TestJsonWriter {
     private static final String pSource = "./data/testPortfolio.json";
     private static final String smSource = "./data/testStockmarket.json";
     private static final String dSource = "./data/testDays.json";
-    private static final String brokenSource = "./data/dne.json";
 
     private String testPortfolioTicker = "testP";
     private int testPortfolioCash = 50;
@@ -27,13 +26,6 @@ public class TestJsonWriter {
     Portfolio p = new Portfolio(testPortfolioCash);
     Stock testStock = new Stock(testSmTicker, "test", testSmPrice, 1);
     StockMarket sm = new StockMarket();
-    int days;
-
-
-    @BeforeEach
-    void setup() {
-
-    }
 
     @Test
     void testWriterInvalidFile() {
@@ -51,11 +43,10 @@ public class TestJsonWriter {
         try {
             p.addStock(testPortfolioTicker, testPortfolioQuantity, testPortfolioPrice);
             sm.addStock(testStock);
-            days = 3;
 
             JsonWriter writer = new JsonWriter(pSource, smSource, dSource);
             writer.open();
-            writer.write(p, sm, days);
+            writer.write(p, sm, testDays);
             writer.close();
 
             JsonReader reader = new JsonReader(pSource, smSource, dSource);
@@ -66,7 +57,7 @@ public class TestJsonWriter {
             assertEquals(p.getCash(), p2.getCash());
             assertEquals(p.getHoldings().get(0).getStockTicker(), p2.getHoldings().get(0).getStockTicker());
             assertEquals(sm.getMarket().size(), sm2.getMarket().size());
-            assertEquals(days, days2);
+            assertEquals(testDays, days2);
 
 
         } catch (IOException e) {
